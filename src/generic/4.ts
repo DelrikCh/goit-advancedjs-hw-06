323113e6c96c771bd31b94037f0ff351bb72b62a
Roman Choporov <ChoporovRoman@gmail.com>
2024-08-23 17:58:12 -0700

    Complete tasks

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,16 +2,18 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
-  constructor (public props:T) {
+class Component<T> {
+  constructor(public props: T) {}
+}
 
+class Page extends Component<PageProps> {
+  pageInfo() {
+    console.log(this.props.title);
   }
 }
 
-class Page extends Component {
-  pageInfo () {
-    console.log(this.props.title);
-  }
+interface PageProps {
+  title: string;
 }
 
 export {};
